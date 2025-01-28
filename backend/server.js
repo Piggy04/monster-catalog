5b18e3c4353ea93f3a7070c3efbb2800d14844f3
Piggy04 <simonetiddi@gmail.com>
2025-01-28 22:17:34 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,72 +1,82 @@
 const express = require('express');
-const cors = require('cors'); // Importa il modulo cors
+const fs = require('fs');
 const path = require('path');
-const fs = require('fs'); // Importa il modulo fs per leggere e scrivere file
+const cors = require('cors'); // Importa il modulo cors
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors()); // Usa il middleware cors
-app.use(express.json()); // Middleware per analizzare il corpo delle richieste JSON
+app.use(express.json()); // Middleware per leggere il JSON
 
 // Percorso per leggere il file delle Monster
 const dataPath = path.join(__dirname, 'data.json');
 
-// Endpoint di base per testare il server
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 // Endpoint per ottenere la lista delle Monster
 app.get('/api/monsters', (req, res) => {
-  fs.readFile(dataPath, 'utf8', (err, data) => {
-    if (err) {
-      return res.status(500).send('Errore nel recupero dei dati');
-    }
-    res.json(JSON.parse(data));
-  });
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Errore nel recupero dei dati');
+        }
+        res.json(JSON.parse(data));
+    });
 });
 
 // Endpoint per aggiornare la lista delle Monster
 app.post('/api/monsters', (req, res) => {
-  const { category, monster, owned } = req.body;
+    const { category, monster, owned } = req.body;
 
-  fs.readFile(dataPath, 'utf8', (err, data) => {
-    if (err) {
-      return res.status(500).send('Errore nel recupero dei dati');
-    }
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Errore nel recupero dei dati');
+        }
 
-    const jsonData = JSON.parse(data);
+        const jsonData = JSON.parse(data);
 
-    // Verifica che la categoria esista
-    if (jsonData[category]) {
-      const index = jsonData[category].findIndex(item => item.name === monster);
-      if (index !== -1) {
-        // Aggiorna lo stato della Monster nella categoria corretta
-        jsonData[category][index].owned = owned;
-        console.log(`Monster aggiornata: ${monster}, Owned: ${owned}`); // Log per debug
-      } else {
-        return res.status(404).send('Monster non trovata nella categoria');
-      }
-    } else {
-      return res.status(404).send('Categoria non trovata');
-    }
+        // Verifica che la categoria esista
+        if (jsonData[category]) {
+            const index = jsonData[category].findIndex(item => item.name === monster);
+            if (index !== -1) {
+                // Aggiorna lo stato della Monster nella categoria corretta
+                jsonData[category][index].owned = owned;
+                console.log(`Monster aggiornata: ${monster}, Owned: ${owned}`); // Log per debug
+            } else {
+                return res.status(404).send('Monster non trovata nella categoria');
+            }
+        } else {
+            return res.status(404).send('Categoria non trovata');
+        }
 
-    // Log per vedere i dati che stiamo salvando
-    console.log('Dati aggiornati:', JSON.stringify(jsonData, null, 2));
-
-    // Salva i nuovi dati nel file
-    fs.writeFile(dataPath, JSON.stringify(jsonData, null, 2), 'utf8', (err) => {
-      if (err) {
-        return res.status(500).send('Errore nel salvataggio dei dati');
-      }
-      console.log('Dati salvati correttamente nel file!'); // Log per confermare
-      res.status(200).send('Dati salvati correttamente');
+        // Salva i nuovi dati nel file
+        fs.writeFile(dataPath, JSON.stringify(jsonData, null, 2), 'utf8', (err) => {
+            if (err) {
+                return res.status(500).send('Errore nel salvataggio dei dati');
+            }
+            res.status(200).send('Dati salvati correttamente');
+        });
     });
-  });
+});
+
+// **Nuovo endpoint**: Ottieni il numero totale di Monster possedute
+app.get('/api/monsters/count', (req, res) => {
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Errore nel recupero dei dati');
+        }
+
+        const jsonData = JSON.parse(data);
+        let totalOwned = 0;
+
+        // Somma tutte le Monster possedute
+        for (const category in jsonData) {
+            totalOwned += jsonData[category].filter(monster => monster.owned).length;
+        }
+
+        res.json({ totalOwned });
+    });
 });
 
 // Avvia il server
 app.listen(port, () => {
-  console.log(`Server in ascolto su http://localhost:${port}`);
+    console.log(`Server in ascolto su http://localhost:${port}`);
 });
